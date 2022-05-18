180aa33d5123e449359eee30261a8125c70478d2
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-05-18 20:20:44 +0100

    first - commit

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(data) {
+  const month = data.date.toLocaleString("eu-US", { month: "long" });
+  const day = data.date.toLocaleString("eu-US", { day: "2-digit" });
+  const year = data.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__mounth">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+function ExpenseItem(data) {
+  
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={data.date}/>
+      <div className="expense-item__description">
+        <h2>{data.title}</h2>
+        <div className="expense-item__price">${data.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
