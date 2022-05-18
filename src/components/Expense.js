92a3f93588d61d901a0e87b9ce3139a27bf5dac1
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-05-18 21:49:26 +0100

    save update

---
+++ src/components/Expense.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expense.css";
+function Expense(data) {
+  
+  return (
+    <div className="expenses">
+      <ExpenseItem 
+      title={data.items[0].title} 
+      amount={data.items[0].amount} 
+      date={data.items[0].date}
+      />
+      <ExpenseItem 
+      title={data.items[1].title} 
+      amount={data.items[1].amount} 
+      date={data.items[1].date}
+      />
+      <ExpenseItem 
+      title={data.items[2].title} 
+      amount={data.items[2].amount} 
+      date={data.items[2].date}
+      />
+      <ExpenseItem 
+      title={data.items[3].title} 
+      amount={data.items[3].amount} 
+      date={data.items[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expense;
