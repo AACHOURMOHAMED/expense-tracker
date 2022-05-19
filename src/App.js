7639939102f127752febf6c1c50f67314654d489
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-05-19 15:07:12 +0100

    update new expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expense from "./components/Expenses/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const  App = () => {
   const expenses = [
@@ -24,8 +25,10 @@ const  App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      
+        <NewExpense/>
         <Expense items={expenses}/>
+        
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,29 @@
+import React,{ useState } from "react";  
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-const  ExpenseItem = (data) => {
+
+const ExpenseItem = (data) => {
   
+  const [title, setTitle] = useState(data.title)
+
+  
+
+  
+  const clickHandler = () => {
+    setTitle('Mohamed');
+  };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={data.date}/>
+      <ExpenseDate date={data.date} />
       <div className="expense-item__description">
-        <h2>{data.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${data.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
